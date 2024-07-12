e0dab4f5fa40a4c6acc9e6770f683c14af986cb7
mloaizau <mixi7395@gmail.com>
2024-07-12 18:41:35 -0400

    formulario de celebraciones

--- src/components/AddBirthday.tsx
+++ src/components/AddBirthday.tsx
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React, { useState } from 'react';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import moment from 'moment';
+
+moment.locale('es');
 
 export const AddBirthday = () => {
 
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
     const [formData, setFormData] = useState({});
+    const [formError, setFormError] = useState({});
 
     const showDatePicker = () => {
         setDatePickerVisibility(true);
@@ -21,9 +24,26 @@ export const AddBirthday = () => {
         dateBirth.setHours(0);
         dateBirth.setMinutes(0);
         dateBirth.setSeconds(0);
-        setFormData({...formData, dateBirth});
+        setFormData({ ...formData, dateBirth });
         hideDatePicker();
     };
+
+    const onChange = (e, type) => {
+        setFormData({ ...formData, [type]: e.nativeEvent.text })
+    }
+
+    const onSubmit = () => {
+        let errors = {};
+        if (!formData.Nombre || !formData.Apellidos || !formData.dateBirth) {
+            if (!formData.Nombre) errors.Nombre = true;
+            if (!formData.Apellidos) errors.Apellidos = true;
+            if (!formData.dateBirth) errors.dateBirth = true;
+        } else {
+            //guardar info
+        }
+
+        setFormError(errors);
+    }
 
     return (
         <>
@@ -31,26 +51,32 @@ export const AddBirthday = () => {
                 <TextInput
                     placeholder='Nombre'
                     placeholderTextColor={"#969696"}
-                    style={styles.input}
+                    style={[styles.input, formError.Nombre && { borderColor: "#940c0c" }]}
+                    onChange={(e) => { onChange(e, "Nombre") }}
                 ></TextInput>
                 <TextInput
                     placeholder='Apellidos'
                     placeholderTextColor={"#969696"}
-                    style={styles.input}
+                    style={[styles.input, formError.Apellidos && { borderColor: "#940c0c" }]}
+                    onChange={(e) => { onChange(e, "Apellidos") }}
                 ></TextInput>
-                <View style={[styles.input, styles.datePicker]}>
-                    <Text style={{color: formData.dateBirth ? '#fff' : '#969696', fontSize: 18}} 
+                <View style={[styles.input, styles.datePicker, formError.dateBirth && { borderColor: "#940c0c" }]}>
+                    <Text
+                        style={{ color: formData.dateBirth ? '#fff' : '#969696', fontSize: 18 }}
                         onPress={showDatePicker}>
-                        { formData.dateBirth ? moment(formData.dateBirth).format("LL")
-                        : 'Fecha de nacimiento' }
+                        {formData.dateBirth ? moment(formData.dateBirth).format("LL")
+                            : 'Fecha de nacimiento'}
                     </Text>
                 </View>
+                <TouchableOpacity onPress={onSubmit}>
+                    <Text style={styles.button}>Crear cumpleaños</Text>
+                </TouchableOpacity>
             </View>
-            <DateTimePickerModal 
-                isVisible={isDatePickerVisible} 
-                mode='date' 
-                onConfirm={handleConfirm} 
-                onCancel={hideDatePicker} 
+            <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode='date'
+                onConfirm={handleConfirm}
+                onCancel={hideDatePicker}
             />
         </>
     )
@@ -78,5 +104,9 @@ const styles = StyleSheet.create({
     },
     datePicker: {
         justifyContent: "center"
+    },
+    button: {
+        fontSize: 18,
+        color: "#000"
     }
 })
