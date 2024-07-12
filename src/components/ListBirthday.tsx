dc5dded8e9cd8a4404700a92da83a8c19328e210
mloaizau <mixi7395@gmail.com>
2024-07-12 19:25:31 -0400

    obteniedo lista de celebraciones

--- src/components/ListBirthday.tsx
+++ src/components/ListBirthday.tsx
@@ -1,23 +1,45 @@
 import { View, Text, StyleSheet } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ActionBar } from './ActionBar';
 import { AddBirthday } from './AddBirthday';
+import firebase from '../utils/firebase';
+import "firebase/compat/firestore";
+
+const db = firebase.firestore();
 
 export const ListBirthday = (props: any) => {
 
     const user = props;
 
     const [showList, setShowList] = useState(true);
+    const [birthday, setBirthday] = useState([]);
+
+    useEffect(() => {
+        setBirthday([]);
+        db.collection(user.user.uid)
+        .orderBy("dateBirth", "asc")
+        .get()
+        .then( (response) => {
+            const itemsArray: any = [];
+            response.forEach((doc) => {
+                const data = doc.data();
+                data.id = doc.id;
+                itemsArray.push(data);
+            });
+            console.log(itemsArray)
+        });
+    }, [])
+
 
     return (
         <View style={styles.container}>
-            { showList ? (
+            {showList ? (
                 <>
                     <Text>ListBirthday</Text>
                 </>
 
             ) : (
-                <AddBirthday user={user} setShowList={setShowList}/>
+                <AddBirthday user={user} setShowList={setShowList} />
             )}
             <ActionBar showList={showList} setShowList={setShowList} />
         </View>
